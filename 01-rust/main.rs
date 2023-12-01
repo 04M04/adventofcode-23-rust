9e462e1d725152c609a1d07b46933e6329891575
04M04 <micsch04@gmail.com>
2023-12-02 00:53:34 +0100

    add main.rs

---
+++ 01-rust/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let input_list = vec![
+        "1abc2",
+        "pqr3stu8vwx",
+        "a1b2c3d4e5f",
+        "treb7uchet"
+    ];
+    
+    for i in &input_list {
+        
+        println!("{}", i);
+    }
+}
