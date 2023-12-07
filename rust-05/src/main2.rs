a7f61d6bfd61d764a718f4a4c816d4bf0f31fc56
04M04 <micsch04@gmail.com>
2023-12-07 04:37:50 +0100

    just a safe

---
+++ rust-05/src/main2.rs
@@ -0,0 +1,32 @@
+// Rust
+const INPUT: &str = include_str!("../input_data.txt");
+
+fn main(){
+    let mut result = 0;
+
+    for line in INPUT.lines() {
+        let (_, line) = line.split_once(": ").unwrap();
+        let draw_parts = line.split("; ");
+
+        
+        let mut max_red_count = 0;
+        let mut max_green_count = 0;
+        let mut max_blue_count = 0;
+        for draw_part in draw_parts {
+            let color_parts = draw_part.split(", ");
+            for color_part in color_parts {
+                // println!("{}", color_part);
+                let (color_count, color) = color_part.split_once(' ').unwrap();
+                let color_count = color_count.parse::<i32>().unwrap();
+                match color {
+                    "red" => {if color_count > max_red_count{max_red_count = color_count}}
+                    "green" => {if color_count > max_green_count{max_green_count = color_count}}
+                    "blue" => {if color_count > max_blue_count{max_blue_count = color_count}}
+                    _ => unreachable!("unknown: {color}")
+                }
+            }
+        }
+        result += max_red_count * max_green_count * max_blue_count;
+    }
+    dbg!(result);
+}
