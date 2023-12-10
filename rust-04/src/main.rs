f290b0e089c4039cb0a213f03a9b628e2f691658
04M04 <micsch04@gmail.com>
2023-12-10 20:07:31 +0100

    just a safe

--- rust-04/src/main.rs
+++ rust-04/src/main.rs
@@ -1,34 +1,43 @@
 // Rust
-const INPUT: &str = include_str!("../input_data.txt");
+const INPUT: &str = include_str!("../test_input_data.txt");
+// const INPUT: &str = include_str!("../input_data.txt");
 
 fn main(){
-    let mut game_id = 0;
     let mut result = 0;
-
-    let red_count = 12;
-    let green_count = 13;
-    let blue_count = 14;
-
+    let mut variable = 0;
     for line in INPUT.lines() {
-        game_id += 1;
-        let mut game_possible = true;
         let (_, line) = line.split_once(": ").unwrap();
-        let draw_parts = line.split("; ");
-        for draw_part in draw_parts {
-            let color_parts = draw_part.split(", ");
-            for color_part in color_parts {
-                // println!("{}", color_part);
-                let (color_count, color) = color_part.split_once(' ').unwrap();
+        
 
-                match color {
-                    "red" => {if color_count.parse::<i32>().unwrap() > red_count{game_possible = false;}}
-                    "green" => {if color_count.parse::<i32>().unwrap() > green_count{game_possible = false;}}
-                    "blue" => {if color_count.parse::<i32>().unwrap() > blue_count{game_possible = false;}}
-                    _ => unreachable!("unknown: {color}")
+        let (numbers_part_1, numbers_part_2) = line.split_once(" | ").unwrap();
+        dbg!(numbers_part_1);
+        dbg!(numbers_part_2);
+        let parts1 = numbers_part_1.split(" ");
+        let parts2 = numbers_part_2.split(" ");
+        
+        for part1 in parts1{
+            for part2 in parts2.clone() {
+                if part1 == part2 && part1 != ""{
+                    result += 1;
+                    variable += 1;
+                    dbg!(variable);
                 }
+                
             }
+            
+            if variable > 0{
+                result += variable;
+                variable = 0;
+            }
+            
+            // for part2 in parts2{
+            //     if part1 == part2{
+            //         
+            //     }
+            // }
+            
+            
         }
-        if game_possible {result += game_id}
     }
     dbg!(result);
 }
--- rust-06/src/main.rs
+++ rust-06/src/main.rs
@@ -4,20 +4,22 @@ const INPUT: &str = include_str!("../input_data.txt");
 fn main(){
     // let mut result = 0;
 
-    let mut time_values = Vec::new();
+    let mut times_and_destinations: Vec<i32> = Vec::new();
 
     for line in INPUT.lines() {
         let (_, line) = line.split_once(":").unwrap();
         let splitted_elems: std::str::Split<'_, &str> = line.split(" ");
 
         for elem in splitted_elems{
-            elem = elem.parse().expect("Not a valid number");
-            if(elem.is_digit(10)){
-                time_values.push(elem.to_digit(10).unwrap());
-                dbg!(time_values);
+            if elem != ""{
+                times_and_destinations.push(elem.parse::<i32>().unwrap());
+                dbg!(elem);
             }
+
             // times.push(elem.parse().unwrap());
-            // dbg!(elem);
+        }
+        for elem in times_and_destinations{
+            dbg!(elem);
         }
     }
     // dbg!(result);
